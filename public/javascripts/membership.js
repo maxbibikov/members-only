67c9137ec76c9c5590bbb52356f6028e0a5b50a5
Maksym Bibikov <bibikovmax@gmail.com>
2020-02-29 13:55:25 +0200

    ✨ add button to set secret string

--- public/javascripts/membership.js
+++ public/javascripts/membership.js
@@ -1,4 +1,13 @@
 const membershipInputEl = document.querySelector('#secret_string');
 if (membershipInputEl) {
-  new mdc.textField.MDCTextField(membershipInputEl);
+  const membershipInput = new mdc.textField.MDCTextField(membershipInputEl);
+
+  const setSecretBtn = document.querySelector('#set_secret_btn');
+  if (setSecretBtn) {
+    new mdc.ripple.MDCRipple(setSecretBtn);
+
+    setSecretBtn.addEventListener('click', () => {
+      membershipInput.value = 'secret string';
+    });
+  }
 }
